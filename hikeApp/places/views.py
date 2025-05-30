b8920105cde14e3de4f80e3423d84ced7d1b7c35
Vlad <vladribak24@gmail.com>
2025-05-30 20:29:24 +0100

    fixed header and pictures from database

--- hikeApp/places/views.py
+++ hikeApp/places/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 
-from .models import Place
 
 
-def locations(request):
-    places = Place.objects.all()
-    return render(request, 'main/index.html', {'places': places})
+
 
