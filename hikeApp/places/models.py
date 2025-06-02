6902a60e8769b7b7c04fb4e76c431dad70d8775b
Vlad <vladribak24@gmail.com>
2025-06-02 10:18:44 +0100

    added description for places

--- hikeApp/places/models.py
+++ hikeApp/places/models.py
@@ -4,6 +4,7 @@ class Place(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=50)
     image = models.ImageField(upload_to='places/')
+    description = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
 
     def __str__(self):
